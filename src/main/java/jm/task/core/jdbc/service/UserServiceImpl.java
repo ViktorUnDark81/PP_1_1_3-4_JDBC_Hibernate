8d8275326639ed2ce9e656cc5d740a871e116c1f
ViktorUnDark81 <Viktor81_81@rambler.ru>
2023-04-28 22:19:51 +0300

    Update UserServiceImpl.java
    commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,17 +1,14 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-<<<<<<< HEAD
-    UserDaoHibernateImpl Dao = new UserDaoHibernateImpl();
-=======
-    UserDaoJDBCImpl Dao = new UserDaoJDBCImpl();
->>>>>>> 4d9432279f8f35b9b21a4f9153a3881d689997fb
+
+    UserDao Dao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
         Dao.createUsersTable();
